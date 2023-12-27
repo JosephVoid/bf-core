2c08d4af809aea3d19d3c561e97a104bfe74f24a
Yoseph <yosephten@gmail.com>
2023-12-27 17:11:22 +0300

    Initial

---
+++ src/test/java/com/buyerfirst/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyerfirst.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
