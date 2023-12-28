b37330bf3643c663d98b09fe1b4c486539995d82
Yoseph <yosephten@gmail.com>
2023-12-28 12:33:22 +0300

    `feat`: added JWT token check on interceptor

--- src/main/java/com/buyersfirst/core/services/RequestInterceptor.java
+++ src/main/java/com/buyersfirst/core/services/RequestInterceptor.java
@@ -1,18 +1,41 @@
 package com.buyersfirst.core.services;
 
+import org.jose4j.jwt.JwtClaims;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.buyersfirst.core.models.UsersRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class RequestInterceptor implements HandlerInterceptor{
+    @Autowired
+    JWTBuilder jwtBuilder;
+    @Autowired
+    UsersRepository usersRepository;
+    
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) {
-        // TODO: JWT Check here
-        return true;
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception{
+        String token = request.getHeader("Authorization");
+
+        if (token == null) {
+            response.getWriter().write("Authorization needed");
+            response.setStatus(401);
+            return false;
+        }
+
+        JwtClaims claims = jwtBuilder.generateParseToken(token);
+        Integer userId = Integer.parseInt(claims.getClaimValue("userId").toString());
+        if (usersRepository.findById(userId).isPresent()) {
+            request.setAttribute("userId", userId);
+            return true;
+        }
+
+        return false;
     }
 
     @Override
