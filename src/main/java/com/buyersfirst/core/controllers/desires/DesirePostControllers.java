58ab6daf2374698c5efcda4530f621bdd083f78c
Yoseph <yosephten@gmail.com>
2023-12-29 16:04:56 +0300

    `feat`: implemented creating deisres

--- src/main/java/com/buyersfirst/core/controllers/desires/DesirePostControllers.java
+++ src/main/java/com/buyersfirst/core/controllers/desires/DesirePostControllers.java
@@ -1,5 +1,8 @@
 package com.buyersfirst.core.controllers.desires;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.buyersfirst.core.interfaces.CreateDesiresRqB;
+import com.buyersfirst.core.models.DesireTags;
+import com.buyersfirst.core.models.DesireTagsRepository;
 import com.buyersfirst.core.models.Desires;
+import com.buyersfirst.core.models.DesiresRepository;
 import com.buyersfirst.core.services.TokenParser;
 
 @RestController
@@ -21,6 +27,10 @@ import com.buyersfirst.core.services.TokenParser;
 public class DesirePostControllers {
     @Autowired
     TokenParser tokenParser;
+    @Autowired
+    DesiresRepository desiresRepository;
+    @Autowired
+    DesireTagsRepository desireTagsRepository;
 
     @PostMapping(path = "/create")
     public @ResponseBody Desires createDesires (@RequestHeader("Authorization") String auth, @RequestBody CreateDesiresRqB request) {
@@ -28,13 +38,33 @@ public class DesirePostControllers {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Input");
         try {
             Integer userId = tokenParser.getUserId(auth);
+            /* Validate input */
+            if (request.title.length() > 100 || request.description.length() > 500)
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Input size limit exceed");
             
-            // TODO: Implement logic
-
-            return new Desires();
+            /* Save desire */
+            Desires desire = new Desires();
+            desire.setCreated(new Timestamp(System.currentTimeMillis()));
+            desire.setTitle(request.title);
+            desire.setDescription(request.description);
+            desire.setDesiredPrice(request.price);
+            desire.setIsClosed(0);
+            desire.setOwnerId(userId);
+            if (request.picture != null)
+                desire.setPicture(request.picture);
+            Desires savedDesire =  desiresRepository.save(desire);
+            
+            /* Save Desire Tag relation */
+            ArrayList<DesireTags> desireTags = new ArrayList<DesireTags>();
+            for (int i = 0; i < request.tags_id.length; i++) {
+                desireTags.add(new DesireTags(request.tags_id[i], savedDesire.getId()));
+            }
+            desireTagsRepository.saveAll(desireTags);
+            
+            return savedDesire;
 
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing Header");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Error or Header Missing");
         }
     }
 
