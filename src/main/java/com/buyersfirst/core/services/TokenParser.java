f36a3a23d358f9e892a113ebb0e981427b3b735a
Yoseph <yosephten@gmail.com>
2024-01-01 17:14:34 +0300

    `auth`: gave a specfic exception for TokenParser

--- src/main/java/com/buyersfirst/core/services/TokenParser.java
+++ src/main/java/com/buyersfirst/core/services/TokenParser.java
@@ -4,12 +4,14 @@ import org.jose4j.jwt.JwtClaims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import jakarta.security.auth.message.AuthException;
+
 @Component
 public class TokenParser {
     @Autowired
     private JWTBuilder jwtBuilder;
 
-    public Integer getUserId(String token) throws Exception{
+    public Integer getUserId(String token) throws AuthException{
         JwtClaims claims = jwtBuilder.generateParseToken(token);
         Integer userId = Integer.parseInt(claims.getClaimValue("userId").toString());
         return userId;
