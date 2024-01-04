47c6913a37e1e91d0606cf21964bfd914695df97
Yoseph <yosephten@gmail.com>
2024-01-04 22:42:05 +0300

    `feat`: added single desire response interface

--- src/main/java/com/buyersfirst/core/interfaces/SingleDesire.java
+++ src/main/java/com/buyersfirst/core/interfaces/SingleDesire.java
@@ -1,5 +1,20 @@
 package com.buyersfirst.core.interfaces;
 
-public class SingleDesire {
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class SingleDesire extends DesireListRsp{
+    public ArrayList<Integer> bidList;
+
+    public SingleDesire(Integer id, String title, String description, String postedBy, Double price, Integer views,
+            Integer wants, Integer bids, boolean isClosed, String picture, Timestamp postedOn, ArrayList<String> tags,
+            ArrayList<Integer> bidList) {
+        super(id, title, description, postedBy, price, views, wants, bids, isClosed, picture, postedOn, tags);
+        this.bidList = bidList;
+    }
+
+    public SingleDesire(ArrayList<Integer> bidList) {
+        this.bidList = bidList;
+    }
     
 }
