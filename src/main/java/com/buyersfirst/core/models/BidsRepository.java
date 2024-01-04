067583e4f65bc5af704cc49ae0713274bed5c571
Yoseph <yosephten@gmail.com>
2024-01-04 22:41:27 +0300

    `feat`: added new query methods

--- src/main/java/com/buyersfirst/core/models/BidsRepository.java
+++ src/main/java/com/buyersfirst/core/models/BidsRepository.java
@@ -1,7 +1,11 @@
 package com.buyersfirst.core.models;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BidsRepository extends CrudRepository<Bids, Integer>{
-
+    @Query("SELECT bd FROM Bids bd where bd.DesireId = ?1")
+    List<Bids> findByDesireId(Integer id);
 }
--- src/main/java/com/buyersfirst/core/models/DesiresRepository.java
+++ src/main/java/com/buyersfirst/core/models/DesiresRepository.java
@@ -51,4 +51,37 @@ public interface DesiresRepository extends CrudRepository<Desires, Integer>{
             CASE WHEN :sortBy = 'desired_price:DESC' THEN desired_price END DESC
         """, nativeQuery = true)
     String[][] findAllDesiresJoined(String filterBy, String sortBy);
+
+    /**
+     * @param filterBy Valid tag names
+     * @param sortBy created, wants, desired_price
+     * @param sortDir ASC or DESC
+     * @return List<Object>
+     */
+    @Query(value = """
+        SELECT 
+            desires.id,
+            users.first_name,
+            users.last_name,
+            desires.title,
+            desires.description,
+            desires.desired_price,
+            desires.picture,
+            desires.created,
+            desires.is_closed,
+            tags.name AS tag_name,
+            COUNT(bids.id) AS bids,
+            COUNT(user_wants.id) AS wants, 
+            COUNT(views.id) AS views 
+        FROM desires
+        LEFT JOIN users ON users.id = desires.owner_id
+        LEFT JOIN bids ON bids.desire_id = desires.id
+        LEFT JOIN user_wants ON user_wants.desire_id = desires.id
+        LEFT JOIN views ON views.desire_id = desires.id
+        RIGHT JOIN desire_tags ON desire_tags.desire_id = desires.id
+        LEFT JOIN tags ON tags.id = desire_tags.tag_id
+        WHERE desires.id = :id
+        GROUP BY desires.id, tags.id
+        """, nativeQuery = true)
+    String[][] findADesireJoined(Integer id);
 }
