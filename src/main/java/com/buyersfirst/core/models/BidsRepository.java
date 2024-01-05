e283c2af5dcf6769ba75c5b8e0006e2e182ba7eb
Yoseph <yosephten@gmail.com>
2024-01-05 09:39:04 +0300

    `feat`: create custom queries

--- src/main/java/com/buyersfirst/core/models/BidsRepository.java
+++ src/main/java/com/buyersfirst/core/models/BidsRepository.java
@@ -8,4 +8,16 @@ import org.springframework.data.repository.CrudRepository;
 public interface BidsRepository extends CrudRepository<Bids, Integer>{
     @Query("SELECT bd FROM Bids bd where bd.DesireId = ?1")
     List<Bids> findByDesireId(Integer id);
+
+    @Query(value = """
+        SELECT bids.*, users.first_name, users.last_name FROM bids
+        JOIN users ON users.id = bids.owner_id
+    """, nativeQuery = true)
+    String[][] findAllBidsJoined();
+
+    @Query(value = """
+        SELECT bids.*, users.first_name, users.last_name FROM bids
+        JOIN users ON users.id = bids.owner_id WHERE bids.id = :id
+    """, nativeQuery = true)
+    String[][] findABidJoined(Integer id);
 }
