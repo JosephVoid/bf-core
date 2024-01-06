b04ce90b8a166325b07ace58f57019de61e1d65a
Yoseph <yosephten@gmail.com>
2024-01-06 15:44:55 +0300

    `feat`: new query methods

--- src/main/java/com/buyersfirst/core/models/AcceptedBidsRepository.java
+++ src/main/java/com/buyersfirst/core/models/AcceptedBidsRepository.java
@@ -1,7 +1,10 @@
 package com.buyersfirst.core.models;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AcceptedBidsRepository extends CrudRepository<AcceptedBids, Integer>{
-
+    @Query("SELECT ab FROM AcceptedBids ab WHERE ab.BidId = ?1 AND ab.UserId = ?2")
+    List<AcceptedBids> findByBidIdAndUserId(Integer BidId, Integer userId);
 }
