0e663f3a3f3edde2449a79d2828afc52eddc5de8
Yoseph <yosephten@gmail.com>
2024-02-11 15:47:37 +0300

    `feat`: the /search endpoint

--- src/main/java/com/buyersfirst/core/controllers/desires/DesireGetControllers.java
+++ src/main/java/com/buyersfirst/core/controllers/desires/DesireGetControllers.java
@@ -34,13 +34,12 @@ public class DesireGetControllers {
     RedisCacheService redisCacheService;
 
     @GetMapping(path = "/all")
-    public @ResponseBody List<DesireListRsp> listDesires (
-        @RequestParam (value = "per-page", defaultValue = "20") Integer perPage,
-        @RequestParam (value = "page", defaultValue = "1") Integer page,
-        @RequestParam (value = "filter-by", defaultValue = "") String filterBy,
-        @RequestParam (value = "sort-by", defaultValue = "wants") String sortBy,
-        @RequestParam (value = "sort-dir", defaultValue = "DESC") String sortDir
-    ) {
+    public @ResponseBody List<DesireListRsp> listDesires(
+            @RequestParam(value = "per-page", defaultValue = "20") Integer perPage,
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "filter-by", defaultValue = "") String filterBy,
+            @RequestParam(value = "sort-by", defaultValue = "wants") String sortBy,
+            @RequestParam(value = "sort-dir", defaultValue = "DESC") String sortDir) {
         if (!(sortBy.equals("created") || sortBy.equals("wants") || sortBy.equals("desired_price")))
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Sort by param invalid");
         if (!(sortDir.equals("ASC") || sortDir.equals("DESC")))
@@ -58,16 +57,19 @@ public class DesireGetControllers {
                 String cachedString = redisCacheService.jedis.get("all-desires");
                 List<DesireListRsp> cachedDesires = mapper.readValue(cachedString, DesireCache.class).allDesires;
 
-                return cachedDesires.subList(perPage*(page - 1), Math.min(cachedDesires.size(), perPage*page));
+                return cachedDesires.subList(perPage * (page - 1), Math.min(cachedDesires.size(), perPage * page));
             }
 
             /* ################################################################### */
 
             /* From DB */
-            String [][] dbResponse = desiresRepository.findAllDesiresJoined(filterBy, sortBy+":"+sortDir);
+            String[][] dbResponse = desiresRepository.findAllDesiresJoined(filterBy, sortBy + ":" + sortDir);
             /* Empty ArrayList for responding */
             ArrayList<DesireListRsp> desires = new ArrayList<DesireListRsp>();
-            /* Empty ArrayList to store the created desires id, used to remove duplicate rows */
+            /*
+             * Empty ArrayList to store the created desires id, used to remove duplicate
+             * rows
+             */
             ArrayList<Integer> createdDesires = new ArrayList<Integer>();
             /* Loop through the 2*2 array for response */
             for (int i = 0; i < dbResponse.length; i++) {
@@ -75,25 +77,23 @@ public class DesireGetControllers {
                 /* If the desire is already created, just append the tag name the desire */
                 if (!createdDesires.contains(Integer.parseInt(Row[0]))) {
                     DesireListRsp dsr = new DesireListRsp(
-                        Integer.parseInt(Row[0]),
-                        Row[3], 
-                        Row[4], 
-                        Row[1]+" "+Row[2], 
-                        Double.parseDouble(Row[5]),
-                        Integer.parseInt(Row[12]),
-                        Integer.parseInt(Row[11]),
-                        Integer.parseInt(Row[10]),
-                        Boolean.parseBoolean(Row[8]),
-                        Row[6],
-                        Timestamp.valueOf(Row[7]),
-                        // For tags
-                        new ArrayList<String>()
-                    );
+                            Integer.parseInt(Row[0]),
+                            Row[3],
+                            Row[4],
+                            Row[1] + " " + Row[2],
+                            Double.parseDouble(Row[5]),
+                            Integer.parseInt(Row[12]),
+                            Integer.parseInt(Row[11]),
+                            Integer.parseInt(Row[10]),
+                            Boolean.parseBoolean(Row[8]),
+                            Row[6],
+                            Timestamp.valueOf(Row[7]),
+                            // For tags
+                            new ArrayList<String>());
                     dsr.tags.add(Row[9]);
                     desires.add(dsr);
                     createdDesires.add(dsr.id);
-                } 
-                else {
+                } else {
                     /* Append the tag name in the desire last desire */
                     desires.get(desires.size() - 1).tags.add(Row[9]);
                 }
@@ -103,11 +103,11 @@ public class DesireGetControllers {
             /* ---------------------CACHING THE RESPONSE-------------------------- */
 
             DesireCache desireCache = new DesireCache(desires);
-            redisCacheService.jedis.setex("all-desires", 3600,mapper.writeValueAsString(desireCache));
+            redisCacheService.jedis.setex("all-desires", 3600, mapper.writeValueAsString(desireCache));
 
             /* ################################################################### */
 
-            return desires.subList(perPage*(page - 1), Math.min(desires.size(), perPage*page));
+            return desires.subList(perPage * (page - 1), Math.min(desires.size(), perPage * page));
 
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
@@ -115,30 +115,29 @@ public class DesireGetControllers {
     }
 
     @GetMapping(path = "/{id}")
-    public @ResponseBody SingleDesire getDesire (@PathVariable Integer id) {
+    public @ResponseBody SingleDesire getDesire(@PathVariable Integer id) {
         try {
             /* From DB */
-            String [][] dbResponse = desiresRepository.findADesireJoined(id);
+            String[][] dbResponse = desiresRepository.findADesireJoined(id);
             if (dbResponse.length < 1)
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Desire doesnt exist");
             /* Create the single desire, without tags or bids */
             SingleDesire dsr = new SingleDesire(
-                Integer.parseInt(dbResponse[0][0]),
-                dbResponse[0][3], 
-                dbResponse[0][4], 
-                dbResponse[0][1]+" "+dbResponse[0][2], 
-                Double.parseDouble(dbResponse[0][5]),
-                Integer.parseInt(dbResponse[0][12]),
-                Integer.parseInt(dbResponse[0][11]),
-                Integer.parseInt(dbResponse[0][10]),
-                Boolean.parseBoolean(dbResponse[0][8]),
-                dbResponse[0][6],
-                Timestamp.valueOf(dbResponse[0][7]),
-                // For Tags
-                new ArrayList<String>(),
-                // For Bid Ids
-                new ArrayList<Integer>()
-            );
+                    Integer.parseInt(dbResponse[0][0]),
+                    dbResponse[0][3],
+                    dbResponse[0][4],
+                    dbResponse[0][1] + " " + dbResponse[0][2],
+                    Double.parseDouble(dbResponse[0][5]),
+                    Integer.parseInt(dbResponse[0][12]),
+                    Integer.parseInt(dbResponse[0][11]),
+                    Integer.parseInt(dbResponse[0][10]),
+                    Boolean.parseBoolean(dbResponse[0][8]),
+                    dbResponse[0][6],
+                    Timestamp.valueOf(dbResponse[0][7]),
+                    // For Tags
+                    new ArrayList<String>(),
+                    // For Bid Ids
+                    new ArrayList<Integer>());
             /* Add the tags on the desire */
             for (int i = 0; i < dbResponse.length; i++) {
                 final String[] Row = dbResponse[i];
@@ -149,6 +148,84 @@ public class DesireGetControllers {
             bids.iterator().forEachRemaining((bid) -> dsr.bidList.add(bid.getId()));
 
             return dsr;
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
+        }
+    }
+
+    @GetMapping(path = "/search")
+    public @ResponseBody List<DesireListRsp> searchDesires(
+            @RequestParam(value = "per-page", defaultValue = "20") Integer perPage,
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "search-string", defaultValue = "") String searchString,
+            @RequestParam(value = "sort-by", defaultValue = "wants") String sortBy,
+            @RequestParam(value = "sort-dir", defaultValue = "DESC") String sortDir) {
+        if (!(sortBy.equals("created") || sortBy.equals("wants") || sortBy.equals("desired_price")))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Sort by param invalid");
+        if (!(sortDir.equals("ASC") || sortDir.equals("DESC")))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Sort dir param invalid");
+        if (page < 1 || perPage < 1)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pagination invalid");
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            /* ################################################################### */
+            /* ------------ SEARCHING AND RESPONDING WITH CACHE ------------------ */
+
+            if (redisCacheService.jedis.exists("all-desires")) {
+                String cachedString = redisCacheService.jedis.get("all-desires");
+                List<DesireListRsp> cachedDesires = mapper.readValue(cachedString, DesireCache.class).allDesires;
+                List<DesireListRsp> searchedDesires = new ArrayList<DesireListRsp>();
+                cachedDesires.forEach((desire) -> {
+                    if (desire.title.toLowerCase().replaceAll("\\s", "")
+                            .contains(searchString.toLowerCase().replaceAll("\\s", "")))
+                        searchedDesires.add(desire);
+                });
+                return searchedDesires.subList(perPage * (page - 1), Math.min(cachedDesires.size(), perPage * page));
+            }
+
+            /* ################################################################### */
+
+            /* From DB */
+            String[][] dbResponse = desiresRepository.searchDesiresJoined(searchString, sortBy + ":" + sortDir);
+            /* Empty ArrayList for responding */
+            ArrayList<DesireListRsp> desires = new ArrayList<DesireListRsp>();
+            /*
+             * Empty ArrayList to store the created desires id, used to remove duplicate
+             * rows
+             */
+            ArrayList<Integer> createdDesires = new ArrayList<Integer>();
+            /* Loop through the 2*2 array for response */
+            for (int i = 0; i < dbResponse.length; i++) {
+                final String[] Row = dbResponse[i];
+                /* If the desire is already created, just append the tag name the desire */
+                if (!createdDesires.contains(Integer.parseInt(Row[0]))) {
+                    DesireListRsp dsr = new DesireListRsp(
+                            Integer.parseInt(Row[0]),
+                            Row[3],
+                            Row[4],
+                            Row[1] + " " + Row[2],
+                            Double.parseDouble(Row[5]),
+                            Integer.parseInt(Row[12]),
+                            Integer.parseInt(Row[11]),
+                            Integer.parseInt(Row[10]),
+                            Boolean.parseBoolean(Row[8]),
+                            Row[6],
+                            Timestamp.valueOf(Row[7]),
+                            // For tags
+                            new ArrayList<String>());
+                    dsr.tags.add(Row[9]);
+                    desires.add(dsr);
+                    createdDesires.add(dsr.id);
+                } else {
+                    /* Append the tag name in the desire last desire */
+                    desires.get(desires.size() - 1).tags.add(Row[9]);
+                }
+            }
+
+            return desires.subList(perPage * (page - 1), Math.min(desires.size(), perPage * page));
 
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
